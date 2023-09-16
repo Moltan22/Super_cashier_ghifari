d1848c0b3b3e85bdb1c24be47da2bdbfbef4c2df
Moltan22 <145213849+Moltan22@users.noreply.github.com>
2023-09-16 22:13:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ modul_market_1.delete_item("Pasta Gigi")
 # Test 3
 modul_market_1.reset_transaction()
 
-# # Test 4
-# modul_market_1.add_item("Ayam Goreng", 2, 20000)
-# modul_market_1.add_item("Pasta Gigi", 3, 15000)
-# modul_market_1.add_item("Mainan Mobil", 1, 200000)
-# modul_market_1.add_item("Mi Instan", 5, 3000)
+# Test 4
+modul_market_1.add_item("Ayam Goreng", 2, 20000)
+modul_market_1.add_item("Pasta Gigi", 3, 15000)
+modul_market_1.add_item("Mainan Mobil", 1, 200000)
+modul_market_1.add_item("Mi Instan", 5, 3000)
 
 print(modul_market_1.check_order())
 print("Total Price:", modul_market_1.total_price())
